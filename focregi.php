40d6eac545a1265a691ce4b6a8f2499dfbd7eda5
SaviruDeSilva <savirurandika25@gmail.com>
2025-07-05 18:05:51 +0530

    initial commit

---
+++ focregi.php
@@ -0,0 +1,26 @@
+<html>
+
+<h4>Registraion Form</h4>
+
+
+
+
+<body>
+
+<form action="focregi1.php"method="post">
+
+
+ Name:  <input type="text" name="name">  <br><br>
+E-mail: <input type="email" name="email">  <br><br>
+Registration No: <input type="text" name="regno" placeholder="SC/2022/XXXX"> <br><br>
+gender   <input type="radio" name="gender" value="male"> <label for="male"> Male </label>
+	<input type="radio" name="gender" value="female"> <label for="female"> Female </label>  <br><br>								
+
+<input type="submit" value="register">
+
+<form>
+
+</body>
+
+
+</html>
